2f1a9dc70fa836f62056b961055213bc1cac8def
Denys Anosov <anosovdenys@gmail.com>
2021-12-07 11:11:07 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  frameworksInfo = [
+    { name: 'Angular', versions: ['1.1.1', '1.2.1', '1.3.3'] },
+    { name: 'React', versions: ['2.1.2', '3.2.4', '4.3.1'] },
+    { name: 'Vue', versions: ['3.3.1', '5.2.1', '5.1.3'] },
+  ];
+
+  selectedFW = 'das';
+
+  frontendEngineerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.frontendEngineerForm = this.fb.group({
+      firstName: fb.control('', [Validators.required]),
+      lastName: fb.control('', [Validators.required]),
+      dateOfBirth: fb.control('', [Validators.required]),
+      framework: fb.control('', [Validators.required]),
+      frameworkVersion: fb.control('', [Validators.required]),
+      email: fb.control('', [Validators.required, Validators.email]),
+      hobby: fb.array([fb.control('', [Validators.required])]),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.frontendEngineerForm.valid) {
+      console.log(this.frontendEngineerForm.value);
+    } else {
+      console.log('not valid');
+    }
+  }
+
+  selectFramework(event: any) {
+    this.frontendEngineerForm.patchValue({
+      framework: event.target.value,
+    });
+    this.selectedFW;
+  }
+
+  addHobby(): void {
+    this.hobby.push(this.fb.control(''));
+  }
+
+  removeHobby(index: number): void {
+    this.hobby.removeAt(index);
+  }
+
+  get hobby(): FormArray {
+    return this.frontendEngineerForm.get('hobby') as FormArray;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
